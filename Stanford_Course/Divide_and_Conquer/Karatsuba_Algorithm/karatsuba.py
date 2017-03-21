37f415c0688da5f3a54e596271b18c3173f57db9
fiatveritas <jesse.gallegos@usc.edu>
2017-03-20 19:48:44 -0700

    feat: added recursive method

--- Stanford_Course/Divide_and_Conquer/Karatsuba_Algorithm/karatsuba.py
+++ Stanford_Course/Divide_and_Conquer/Karatsuba_Algorithm/karatsuba.py
@@ -7,17 +7,26 @@ def divider(number, counter = 0):
 		counter += 1
 		return divider(number / 10, counter = counter)
 def karatsuba(x, y):
-	x_1, m = divider(number = x)
-	x_0 = x - x_1 * 10**m
-	print str(x), '=', str(x_1), '* 10^', str(m), '+',str(x_0)
+	if (x < 10) or (y < 10):
+		return x * y
+	elif (x >= 10) and (y >= 10):
+		x_1, m = divider(number = x)
+		x_0 = x - x_1 * 10**m
+		#print str(x), '=', str(x_1), '* 10^', str(m), '+',str(x_0)
 
-	y_1, m = divider(number = y)
-	y_0 = y - y_1 * 10**m
-	print str(y), '=', str(y_1), '* 10^', str(m), '+',str(y_0)	
-	return None
+		y_1, m = divider(number = y)
+		y_0 = y - y_1 * 10**m
+		#print str(y), '=', str(y_1), '* 10^', str(m), '+',str(y_0)
+		z_2 = x_1 * y_1
+		z_0 = karatsuba(x_0, y_0)
+		z_1 = (x_1 + x_0) * (y_1 + y_0) - z_2 - z_0
+	return z_2*10**(2*m) + z_1*10**m + z_0
+
 if __name__ == '__main__':
 	numbers_file = open('numbers.txt', 'r')
 	lines = [int(i) for i in numbers_file.read().split()]
 	number_1 = lines[0]
 	number_2 = lines[1]
-	karatsuba(x = number_1, y = number_2)
+	#print karatsuba(x = number_1, y = number_2)
+	#print number_1 * number_2
+print karatsuba(201,113)
