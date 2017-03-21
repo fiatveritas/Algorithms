08cfc259adb695566b509f8916ad9ecc40b60252
Jesse Gallegos <gllgsjss@gmail.com>
2017-03-21 09:06:21 -0700

    feat: corrected procedure

--- Stanford_Course/Divide_and_Conquer/Karatsuba_Algorithm/karatsuba.py
+++ Stanford_Course/Divide_and_Conquer/Karatsuba_Algorithm/karatsuba.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python2.7.11
+import numpy as np
 def divider(number, counter = 0):
 	if number / 10 < 10:
 		counter += 1
@@ -7,26 +8,30 @@ def divider(number, counter = 0):
 		counter += 1
 		return divider(number / 10, counter = counter)
 def karatsuba(x, y):
+	#print x, y
 	if (x < 10) or (y < 10):
 		return x * y
 	elif (x >= 10) and (y >= 10):
-		x_1, m = divider(number = x)
-		x_0 = x - x_1 * 10**m
-		#print str(x), '=', str(x_1), '* 10^', str(m), '+',str(x_0)
+		length = max(len(str(x)), len(str(y)))
+		div_by_2 = length / 2
 
-		y_1, m = divider(number = y)
-		y_0 = y - y_1 * 10**m
-		#print str(y), '=', str(y_1), '* 10^', str(m), '+',str(y_0)
-		z_2 = x_1 * y_1
-		z_0 = karatsuba(x_0, y_0)
-		z_1 = (x_1 + x_0) * (y_1 + y_0) - z_2 - z_0
-	return z_2*10**(2*m) + z_1*10**m + z_0
+		x1 = x / (10 **(div_by_2))
+		x0 = x % (10 **(div_by_2))
+		#print str(x), '=', str(x_1), '* 2^', str(m), '+',str(x_0)
+		y1 = y / (10 **(div_by_2))
+		y0 = y % (10 **(div_by_2))
+		#print str(y), '=', str(y_1), '* 2^', str(m), '+',str(y_0)
+		z2 = x1 * y1
+		z0 = karatsuba(x0, y0)
+		z1 = karatsuba(x1 + x0, y1 + y0) - z2 - z0
+	return z2*10**(2*div_by_2) + z1*10**div_by_2 + z0
 
 if __name__ == '__main__':
 	numbers_file = open('numbers.txt', 'r')
 	lines = [int(i) for i in numbers_file.read().split()]
 	number_1 = lines[0]
 	number_2 = lines[1]
-	#print karatsuba(x = number_1, y = number_2)
-	#print number_1 * number_2
-print karatsuba(201,113)
+	print number_1
+	print number_2
+	print karatsuba(x = number_1, y = number_2)
+	
