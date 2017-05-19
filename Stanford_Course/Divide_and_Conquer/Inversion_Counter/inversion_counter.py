626ed2179acc130e15202c7a66ddb3fdd6fabd1e
Jesse Gallegos <gllgsjss@gmail.com>
2017-05-18 18:30:45 -0700

    feat: redid code and ran

--- Stanford_Course/Divide_and_Conquer/Inversion_Counter/inversion_counter.py
+++ Stanford_Course/Divide_and_Conquer/Inversion_Counter/inversion_counter.py
@@ -1,45 +1,61 @@
 #!/usr/bin/python2.7
-def merge_sort(alist):
-    counter = 0
-    if len(alist) > 1:
-        mid = len(alist) // 2
-        lefthalf = alist[:mid]
-        righthalf = alist[mid:]
+def merge_sort(to_sort):
+    if len(to_sort) > 1:
+        [left_half, right_half] = divide(to_sort)
+        #print x1,x2
+        left_half, leftInv = merge_sort(left_half)
+        right_half, rightInv = merge_sort(right_half)
+        #print leftInv,rightInv
+        merged, splitInv = merge(left_half, right_half)
+        #print splitInv
+        return merged,(leftInv + rightInv + splitInv)
+    else:
+        return to_sort, 0
 
-        merge_sort(lefthalf)
-        merge_sort(righthalf)
 
-        i = 0
-        j = 0
-        k = 0
+def divide(to_sort):
+    size = len(to_sort)
+    half_size = size / 2
+    left_half = []
+    right_half = []
+    for index in xrange(0, half_size):
+        left_half.append(to_sort[index])
+    for index in xrange(half_size, size):
+        right_half.append(to_sort[index])
+    return [left_half, right_half]
 
-        while i < len(lefthalf) and j < len(righthalf):
-            if lefthalf[i] < righthalf[j]:
-                alist[k] = lefthalf[i]
-                i += 1
-            else:
-                alist[k] = righthalf[j]
-                j += 1
-            k += 1
 
-        while i < len(lefthalf):
-            alist[k] = lefthalf[i]
-            i += 1
-            k += 1
+def merge(left_half, right_half):
+    sorted_list = []
+    left_half_index = 0
+    right_half_index = 0
+    inv_count = 0
+    size1 = len(left_half)
+    size2 = len(right_half)
+    while (left_half_index < size1 and right_half_index < size2):
+        if left_half[left_half_index] <= right_half[right_half_index]:
+            sorted_list.append(left_half[left_half_index])
+            left_half_index += 1
+        else:
+            sorted_list.append(right_half[right_half_index])
+            right_half_index += 1
+            inv_count += size1 - left_half_index
 
-        while j < len(righthalf):
-            alist[k] = righthalf[j]
-            j += 1
-            k += 1
+    for i in range(left_half_index, size1):
+         sorted_list.append(left_half[i])
 
+    for i in range(right_half_index, size2):
+         sorted_list.append(right_half[i])
+
+    return sorted_list, inv_count
 
 #alist = [54,26,93,17,77,31,44,55,20]
-#merge_sort(alist)
+#print merge_sort(alist)
 #print(alist)
 
 if __name__ == "__main__":
     numbers_file = open('integerArray.txt', 'r')
     unsorted_list = [int(i) for i in numbers_file.read().split()]
     print merge_sort(unsorted_list)
-    print unsorted_list
+    #print unsorted_list
     numbers_file.close()
