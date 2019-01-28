3ed6cd1a3f2bcebd62ec60e5952b18c65f417a4e
fiatveritas <jesse.gallegos@usc.edu>
2019-01-27 16:04:05 -0800

    feat: opened new file...need to parse file

--- Stanford_Course/Greedy_Algorithms_Minimum_Spanning_Trees_and_Dynamic_Programming/greedy_algorithm_difference.py
+++ Stanford_Course/Greedy_Algorithms_Minimum_Spanning_Trees_and_Dynamic_Programming/greedy_algorithm_difference.py
@@ -2,7 +2,7 @@
 import random #for randomizing lists
 import time
 
-def read_file():
+def read_file_weigth_length():
 	f = open('jobs.txt', 'r')
 	if f:
 		print("file read!")
@@ -12,6 +12,21 @@ def read_file():
 		lines = [(int(line.split()[0]), int(line.split()[1]), int(line.split()[0]) - int(line.split()[1]), int(line.split()[0]) / int(line.split()[1])) for line in lines]
 		f.close()
 		return num_jobs, lines
+	else:
+		f.close()
+
+def read_file_graph():
+	f = open('edges.txt', 'r')
+	if f:
+		print("file read!")
+		lines = f.readlines()
+		nodes, edges = int(lines[0].split()[0]), int(lines[0].split()[1])
+		print(nodes, edges)
+		lines = lines[1:]
+		print(lines)
+		#lines = [(int(line.split()[0]), int(line.split()[1]), int(line.split()[0]) - int(line.split()[1]), int(line.split()[0]) / int(line.split()[1])) for line in lines]
+		f.close()
+		return nodes, edges, lines
 	else:
 		f.close()
 
@@ -38,17 +53,22 @@ def length_now(tuple_passed, length_so_far):
 	return length_so_far + tuple_passed[1]
 
 if __name__ == "__main__":
+	"""###################
 	start = time.time()
-	num_jobs, weights_lengths = read_file()
+	num_jobs, weights_lengths = read_file_weigth_length()
 	sorted_array = sorted(sorted(weights_lengths, key = by_weight, reverse = True), key = by_diff, reverse = True)
 	end = time.time()
 	print("run time:", end - start)
 	print("at last", weighed_sum(sorted_array))
+	###################
 	start = time.time()
 	sorted_array = sorted(sorted(weights_lengths, key = by_weight, reverse = True), key = by_ratio, reverse = True)
 	end = time.time()
 	print("run time:", end - start)
 	print("at last", weighed_sum(sorted_array))
+	###################"""
+	nodes, edges, graph = read_file_graph()
+
 
 """This file describes a set of jobs with positive and
 integral weights and lengths. It has the format
@@ -85,3 +105,36 @@ test cases to debug your algorithm (and post your test
 #69119377652
 #67311454237
 #
+
+"""This file describes an undirected graph with integer edge
+costs. It has the format
+
+[number_of_nodes] [number_of_edges]
+
+[one_node_of_edge_1] [other_node_of_edge_1] [edge_1_cost]
+
+[one_node_of_edge_2] [other_node_of_edge_2] [edge_2_cost]
+
+...
+
+For example, the third line of the file is "2 3 -8874",
+indicating that there is an edge connecting vertex #2 and
+vertex #3 that has cost -8874. You should NOT assume that
+edge costs are positive, nor should you assume that they
+are distinct. Your task is to run Prim's minimum spanning
+tree algorithm on this graph. You should report the overall
+cost of a minimum spanning tree --- an integer, which may
+or may not be negative --- in the box below.
+
+IMPLEMENTATION NOTES: This graph is small enough that the
+straightforward O(mn) time implementation of Prim's algorithm
+should work fine. OPTIONAL: For those of you seeking an additional
+challenge, try implementing a heap-based version. The simpler
+approach, which should already give you a healthy speed-up, is
+to maintain relevant edges in a heap (with keys = edge costs).
+The superior approach stores the unprocessed vertices in the heap,
+as described in lecture. Note this requires a heap that supports
+deletions, and you'll probably need to maintain some kind of
+mapping between vertices and their positions in the heap.
+
+"""
