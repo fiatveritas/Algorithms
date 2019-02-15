105f61053d8ef0e1acde155cf0fc4bfdc51474fe
fiatveritas <jesse.gallegos@usc.edu>
2019-02-14 22:58:57 -0800

    feat: redid code...finishes but result not correct

--- Stanford_Course/Greedy_Algorithms_Minimum_Spanning_Trees_and_Dynamic_Programming/greedy_algorithm_difference.py
+++ Stanford_Course/Greedy_Algorithms_Minimum_Spanning_Trees_and_Dynamic_Programming/greedy_algorithm_difference.py
@@ -97,7 +97,73 @@ def create_graph(list_of_tuples):
 
 def primm_algorithm(graph, starting_node, seen, not_seen, queue, min_span_tree):
 	while not_seen:
-		#
+		print("xxxxxxxxxxxxxxxx")
+		print("starting_node:", starting_node)
+		seen.append(starting_node)
+		#seen.sort()
+		not_seen.remove(starting_node)
+		#print("================")
+		#print("seen:", seen)
+		queue = update_queue(graph, starting_node, seen, queue)
+		#print("================")
+		#print("queue:", queue)
+		print("================")
+		min_edge = min_tuple(queue)
+		print("min_tuple:", min_edge)
+		#print("================")
+		queue = clean_up(min_edge, queue)
+		#print("clean_up:", queue)
+		print("================")
+		starting_node = new_start(min_edge, seen, not_seen, queue)
+		if not starting_node:
+			print("starting_node:", starting_node)
+			#print("length of seen:", len(set(seen)), seen)
+			if seen == list(range(1,501)):
+				print("all nodes seen")
+			#print("length of not seen:", len(not_seen), not_seen)
+			#print(queue)
+			return min_span_tree
+		print("starting_node", starting_node)
+		print("================")
+		min_span_tree.append(min_edge)
+		#print("min_span_tree:", min_span_tree)
+		#print("len tree:", len(min_span_tree))
+	return min_span_tree
+
+def update_queue(graph, starting_node, seen, queue):
+	for i in graph[starting_node]:
+		if i not in queue and (i[0] not in seen or i[1] not in seen):
+			queue.append(i)
+	return queue
+
+def min_tuple(queue):
+	holder = ()
+	weight = math.inf
+	for i in queue:
+		if not holder:
+			holder = i
+			weight = i[2]
+		if i[2] < weight:
+			holder = i
+			weight = i[2]
+	return holder
+
+def clean_up(min_edge, queue):
+	queue.remove(min_edge)
+	queue.remove((min_edge[1], min_edge[0], min_edge[2]))
+	return queue
+
+def new_start(min_edge, seen, not_seen, queue):
+	if min_edge[1] not in seen:
+		return min_edge[1]
+	elif min_edge[0] not in seen:
+		return min_edge[0]
+	else:
+		for i in queue:
+			if i[0] in not_seen:
+				return i[0]
+			elif i[1] in not_seen:
+				return i[1]
 
 def length_now(tuple_passed, length_so_far):
 	return length_so_far + tuple_passed[1]
@@ -106,7 +172,7 @@ if __name__ == "__main__":
 	#master_cost()
 	###################
 	nodes, edges, graph = read_file_graph()
-	random.seed(a = 0)
+	random.seed(a = 1)
 	starting_node = random.choice(list(graph.keys()))
 	seen = []
 	not_seen = list(graph.keys())
@@ -118,81 +184,13 @@ if __name__ == "__main__":
 	#print("# of edges", edges)
 	print("xxxxxxxxxxxxxxxx")
 	min_span_tree = primm_algorithm(graph, starting_node, seen, not_seen, queue, min_span_tree)
-	print(min_span_tree)
-	#print(len(min_span_tree))
-	#sum = 0
-	"""for i in min_span_tree:
+	#print(min_span_tree)
+	print("length of min_span_tree:", len(min_span_tree))
+	sum = 0
+	for i in min_span_tree:
 		sum += i[2]
-	print(sum)"""
+	print(sum)
 
-
-#random.choice()
-
-"""This file describes a set of jobs with positive and
-integral weights and lengths. It has the format
-
-[number_of_jobs]
-
-[job_1_weight] [job_1_length]
-
-[job_2_weight] [job_2_length]
-
-...
-
-For example, the third line of the file is "74 59",
-indicating that the second job has weight 74 and
-length 59.
-
-You should NOT assume that edge weights or lengths are
-distinct.
-
-Your task in this problem is to run the greedy algorithm
-that schedules jobs in decreasing order of the difference
-(weight - length). Recall from lecture that this
-algorithm is not always optimal. IMPORTANT: if two jobs
-have equal difference (weight - length), you should schedule
-the job with higher weight first. Beware: if you break ties
-in a different way, you are likely to get the wrong answer.
-You should report the sum of weighted completion times of
-the resulting schedule --- a positive integer --- in the
-box below.
-
-ADVICE: If you get the wrong answer, try out some small
-test cases to debug your algorithm (and post your test
-	cases to the discussion forum)."""
 #69119377652
 #67311454237
-#
-
-"""This file describes an undirected graph with integer edge
-costs. It has the format
-
-[number_of_nodes] [number_of_edges]
-
-[one_node_of_edge_1] [other_node_of_edge_1] [edge_1_cost]
-
-[one_node_of_edge_2] [other_node_of_edge_2] [edge_2_cost]
-
-...
-
-For example, the third line of the file is "2 3 -8874",
-indicating that there is an edge connecting vertex #2 and
-vertex #3 that has cost -8874. You should NOT assume that
-edge costs are positive, nor should you assume that they
-are distinct. Your task is to run Prim's minimum spanning
-tree algorithm on this graph. You should report the overall
-cost of a minimum spanning tree --- an integer, which may
-or may not be negative --- in the box below.
-
-IMPLEMENTATION NOTES: This graph is small enough that the
-straightforward O(mn) time implementation of Prim's algorithm
-should work fine. OPTIONAL: For those of you seeking an additional
-challenge, try implementing a heap-based version. The simpler
-approach, which should already give you a healthy speed-up, is
-to maintain relevant edges in a heap (with keys = edge costs).
-The superior approach stores the unprocessed vertices in the heap,
-as described in lecture. Note this requires a heap that supports
-deletions, and you'll probably need to maintain some kind of
-mapping between vertices and their positions in the heap.
-
-"""
+#-3624209, -3795206
