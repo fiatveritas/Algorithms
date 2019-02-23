3ba37975f13480fd91fe2b8286239f82a7ad5b19
fiatveritas <jesse.gallegos@usc.edu>
2019-02-22 22:49:59 -0800

    feat: fixed mistake and got correct output

--- Stanford_Course/Greedy_Algorithms_Minimum_Spanning_Trees_and_Dynamic_Programming/greedy_algorithm_difference.py
+++ Stanford_Course/Greedy_Algorithms_Minimum_Spanning_Trees_and_Dynamic_Programming/greedy_algorithm_difference.py
@@ -63,7 +63,7 @@ def master_cost():
 	compute_ratio_cost(weights_lengths)
 
 def read_file_graph():
-	file_name = 'edges_2.txt'
+	file_name = 'edges.txt'
 	lines = open_file(file_name)
 	nodes, edges = int(lines[0].split()[0]), int(lines[0].split()[1])
 	lines = create_edges(lines[1:])
@@ -105,70 +105,45 @@ def primm_algorithm(graph, starting_node):
 	holder = []
 
 	while not_seen:
-		print("xxxxxxxxxxxxxxxx")
-		print("starting_node:", starting_node)
 		seen.append(starting_node)
-		#seen.sort()
 		not_seen.remove(starting_node)
-		#print("================")
-		#print("graph[starting_node]:", graph[starting_node])
-		#print("================")
-		#print("seen:", seen)
 		queue = update_queue(graph, starting_node, seen, queue, detritus)
-		print("================")
-		print("queue:", queue)
-		print("================")
-		min_edge = min_tuple(queue)
+		min_edge = min_tuple(starting_node, seen, queue)
+		if not min_edge:
+			seen.sort()
+			if seen == list(range(1,501)):
+				print("all nodes seen")
+			return min_span_tree
 		if min_edge not in holder:
 			holder.append(min_edge)
 			holder.append((min_edge[1], min_edge[0], min_edge[2]))
 		detritus = set(holder)
-		print("min_tuple:", min_edge)
-		print("================")
 		queue = clean_up(queue, detritus)
-		print("clean_up:", queue)
-		print("================")
 		starting_node = new_start(min_edge, seen, queue)
-		if not starting_node:
-			print("starting_node:", starting_node)
-			print("length of seen:", len(set(seen)), seen)
-			seen.sort()
-			if seen == list(range(1,501)):
-				print("all nodes seen")
-			print("length of not seen:", len(not_seen), not_seen)
-			#print(queue)
-			return min_span_tree
-		print("starting_node", starting_node)
-		#print("================")
 		min_span_tree.append(min_edge)
-		#print("min_span_tree:", min_span_tree)
-		#print("len tree:", len(min_span_tree))
 	return min_span_tree
 
 def update_queue(graph, starting_node, seen, queue, detritus):
 	for i in graph[starting_node]:
 		if i not in queue:
 			queue.append(i)
-	return list(set(queue) - detritus)
+	return sorted(list(set(queue) - detritus), key = by_third)
 
-def min_tuple(queue):
-	return min(queue, key = by_third)
+def min_tuple(starting_node, seen, queue):
+	for i in queue:
+		if i[0] not in seen:
+			return i
+		if i[1] not in seen:
+			return i
 
 def clean_up(queue, detritus):
-	return list(set(queue) - detritus)
+	return sorted(list(set(queue) - detritus), key = by_third)
 
 def new_start(min_edge, seen, queue):
 	if min_edge[1] not in seen:
 		return min_edge[1]
 	if min_edge[0] not in seen:
 		return min_edge[0]
-	for i in sorted(queue, key = by_third):
-		if i[0] not in seen:
-			print("new_start:", i)
-			return i[0]
-		if i[1] not in seen:
-			print("new_start:", i)
-			return i[1]
 
 def by_third(item):
 	return item[2]
@@ -180,20 +155,17 @@ if __name__ == "__main__":
 	#master_cost()
 	###################
 	nodes, edges, graph = read_file_graph()
-	random.seed(a = 0)
-	starting_node = random.choice(list(graph.keys()))
-	#print("graph:", graph)
-	#print("keys:", list(graph.keys()))
-	#print("# of nodes:", nodes)
-	#print("# of edges", edges)
-	print("xxxxxxxxxxxxxxxx")
-	min_span_tree = primm_algorithm(graph, starting_node)
-	#print(min_span_tree)
-	print("length of min_span_tree:", len(set(min_span_tree)))
-	sum = 0
-	for i in min_span_tree:
-		sum += i[2]
-	print(sum)
+	for i in range(0, 10):
+		random.seed(a = i)
+		starting_node = random.choice(list(graph.keys()))
+		print("xxxxxxxxxxxxxxxx")
+		print("seed:", i)
+		min_span_tree = primm_algorithm(graph, starting_node)
+		print("length of min_span_tree:", len(set(min_span_tree)))
+		sum = 0
+		for i in min_span_tree:
+			sum += i[2]
+		print(sum)
 
 #69119377652
 #67311454237
